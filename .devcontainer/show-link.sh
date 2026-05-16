fa39962d723158d7c3f9216ba0e1594e53e9a145
Amir is super sexy <amirmyhusband13@gmail.com>
2026-05-16 04:32:30 +0330

    Update show-link.sh

--- .devcontainer/show-link.sh
+++ .devcontainer/show-link.sh
@@ -2,19 +2,11 @@
 
 CONFIG="/etc/config.json"
 
-UUID=$(jq -r '.inbounds[0].settings.clients[0].id' "$CONFIG")
-
-if [ -z "$UUID" ] || [ "$UUID" = "null" ]; then
-  echo "[g2ray] UUID not found."
-  exit 1
-fi
 
 SNI="${CODESPACE_NAME}-443.app.github.dev"
 
 
-
-
-LINK="vless://${UUID}@85.10.207.48:443?encryption=none&security=tls&type=xhttp&mode=packet-up&sni=${SNI}&path=%2F#AmiR"
+LINK="vless://425b979f-56da-40d9-95d0-c42008b6bf0f@85.10.207.48:443?encryption=none&security=tls&type=xhttp&mode=packet-up&sni=${SNI}&path=%2F#AmiR"
 echo ""
 echo "====================AMIR====================="
 echo "  $LINK"
