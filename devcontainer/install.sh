e106eac64415df3139f74faa7fe637664a56add1
Amir is super sexy <amirmyhusband13@gmail.com>
2026-05-16 03:35:00 +0330

    Add files via upload

---
+++ devcontainer/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+echo "[install] downloading xray..."
+
+wget -O /tmp/xray.zip https://github.com/XTLS/Xray-core/releases/download/v26.3.27/Xray-linux-64.zip
+
+echo "[install] extracting..."
+
+unzip /tmp/xray.zip -d /tmp/xray
+
+chmod +x /tmp/xray/xray
+mv /tmp/xray/xray /usr/local/bin/xray
+
+echo "[install] cleanup..."
+
+rm -rf /tmp/xray /tmp/xray.zip
+
+echo "[install] installed successfully AMIR"
