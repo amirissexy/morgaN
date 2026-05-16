9768b1a51cb63007c1dabdf8179eaeebaf5a7893
Amir is super sexy <amirmyhusband13@gmail.com>
2026-05-16 04:30:41 +0330

    Update start.sh

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -2,18 +2,6 @@
 
 echo "[g2ray] Starting..."
 
-# Generate UUID
-UUID=$(node -e "console.log(crypto.randomUUID())")
-
-echo "[g2ray] UUID: $UUID"
-
-# Inject UUID into config
-jq --arg uuid "$UUID" '
-  .inbounds[0].settings.clients[0].id = $uuid
-' /etc/config.json > /tmp/config.json && mv /tmp/config.json /etc/config.json
-
-echo "[g2ray] Config updated"
-
 # Start tmux session
 tmux kill-session -t g2ray 2>/dev/null || true
 tmux new-session -d -s g2ray
